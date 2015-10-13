c6fd19bdbfdf2dae045883081d14c6a2608549a1
Kai Hendry <hendry@webconverger.com>
2015-10-13 15:00:22 +0800

    Using vanilla XHR

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
-fetch('http://data.hazewatchapp.com/index_v2.json')
-.then(function(response) {
-	return response.json()
-}).then(function(json) {
-	// console.log('parsed json', json)
+// TODO: Remove once Safari supports fetch
+function fetchJSONFile(path, callback) {
+	var httpRequest = new XMLHttpRequest();
+	httpRequest.onreadystatechange = function() {
+		if (httpRequest.readyState === 4) {
+			if (httpRequest.status === 200) {
+				var data = JSON.parse(httpRequest.responseText);
+				if (callback) callback(data);
+			}
+		}
+	};
+	httpRequest.open('GET', path);
+	httpRequest.send();
+}
+
+fetchJSONFile('http://data.hazewatchapp.com/index_v2.json', function(json){
 		for (var i = 0; i < json.result.length; i++) {
 			if (json.result[i].lokasi == "Petaling Jaya") {
 				console.log(json.result[i]);
@@ -11,8 +22,8 @@ fetch('http://data.hazewatchapp.com/index_v2.json')
 					template: '#template',
 					data: json.result[i]
 				});
+			break;
 			}
+			console.log(json.result[i]);
 		}
-}).catch(function(ex) {
-	console.log('parsing failed', ex)
-})
+		});
